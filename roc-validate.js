01ab6bf81a5a41664b0ed15529cad59983011aa6
Peter Sefton <pt@ptsefton.com>
2023-09-28 09:43:16 +1000

    Basic commandline working

--- roc-validate.js
+++ roc-validate.js
@@ -1,49 +1,53 @@
 #!/usr/bin/env node
 
-
 const program = require('commander');
 const fs = require('fs/promises');
 const path = require('path');
-const {ROCrate} = require("./lib/rocrate");
-const {Validator} = require("./lib/validator");
+const { ROCrate } = require('./lib/rocrate');
+const { Validator } = require('./lib/validator');
 
 var crateDir;
 
 program
-  .version("0.1.0")
-  .description(
-    "Runs a minimal RO-Crate validation"
+  .version('0.1.0')
+  .description('Runs a minimal RO-Crate validation')
+  .option('-f, --files <csv-path>', 
+      'Path to a csv file into which the tool will write a summary of which files are in the crate directory and mentioned in the crate.',
   )
-  .arguments("<dir>")
-  .action((dir) => { crateDir = dir })
-
-
+  .arguments('<dir>')
+  .action((dir) => {
+    crateDir = dir;
+  })
 
 program.parse(process.argv);
 const outPath = program.outputPath ? program.outputPath : crateDir;
 
-
-
 async function main() {
-  const rawJson = await fs.readFile(path.join(crateDir, "ro-crate-metadata.json"), 'utf8');
-  
-  const validator = new Validator(rawJson);
-  await validator.validate()
+  const rawJson = await fs.readFile(
+    path.join(crateDir, 'ro-crate-metadata.json'),
+    'utf8'
+  );
+  const validator = new Validator();
+  validator.parseJSON(rawJson);
+  await validator.validate();
 
-  const files = await fs.readdir(crateDir, {recursive: true})
-  const filesObj = Object.fromEntries(files.map(value => [value, {exists: true, inCrate: false}]))
-  console.log(filesObj)
+  if (program.files) {
+    const files = await fs.readdir(crateDir, { recursive: true });
+    // Initialise a files object which has all the files found in the crate
+    const filesObj = Object.fromEntries(
+      files.map((value) => [value, { exists: true, inCrate: false }])
+    );
+    validator.checkFiles(filesObj);
 
-  validator.checkFiles(filesObj);
-  console.log(console.log(validator.result.warnings));
-
-  var csvString = "file,exists,inCrate\n"
-  for (let key of Object.keys(filesObj)) {
-    csvString += `"${key.replace(/([,"])/g, "$1$1")}",${filesObj[key].exists},${filesObj[key].inCrate}\n`
+    var csvString = 'file,exists,inCrate\n';
+    for (let key of Object.keys(filesObj)) {
+      csvString += `"${key.replace(/([,"])/g, '$1$1')}",${
+        filesObj[key].exists
+      },${filesObj[key].inCrate}\n`;
+    }
+    fs.writeFile(program.files, csvString);
   }
-  fs.writeFile("file-summary.csv", csvString)
-  
+  console.log(validator.result)
 }
 
 main();
-
---
+++ validation/README.md
@@ -0,0 +1,15 @@
+# Validation tools
+
+RO-Crate-js has a basic RO-Crate validation tool as part of the library. RO-Crate is a very relaxed and permissive specification, so there are only a few requirements that count as validation errors - though the validator will also issue some warnings and some summary information.
+
+## Install
+
+To use validation from the command line.
+
+-  Clone this repository
+-  Install: `npm install .`
+-  Enable the scripts: `npm link .`
+
+## Usage
+
+
