146f1c892cc880a83bcd1c91394b78d590af18c8
Alvin Sebastian <a.sebastian@connect.qut.edu.au>
2024-08-08 00:49:05 +0000

    Expose Validator class and validate function

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const {ROCrate} = require('./lib/rocrate.js');
 const {Checker} = require('./lib/checker.js');
 const {Utils} = require('./lib/utils.js');
+const {Validator, validate} = require('./lib/validator.js');
 const Defaults = require('./lib/defaults.js');
 
 module.exports = {
   ROCrate,
   Checker,
   Utils,
+  Validator,
+  validate,
   Defaults,
 };
 
